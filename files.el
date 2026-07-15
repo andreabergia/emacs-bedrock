7c27cc7d462790c18509635efdaa1899aab7411d
andrea.bergia <andrea.bergia@servicenow.com>
2026-07-15 14:07:06 +0200

    Added desktop mode

--- files.el
+++ files.el
@@ -6,6 +6,9 @@
 
 (save-place-mode)
 
+;; restore open buffers and window layout from the previous session
+(desktop-save-mode 1)
+
 ;; colorize dired listings (directories, symlinks, dates, file types)
 (use-package diredfl
   :ensure t
