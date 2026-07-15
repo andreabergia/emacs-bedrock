bc3c8024facbd9afd28c3692d45de702b1330f34
andrea.bergia <andrea.bergia@servicenow.com>
2026-07-15 12:48:07 +0200

    Better-looking dired

--- files.el
+++ files.el
@@ -5,3 +5,16 @@
 (recentf-mode)
 
 (save-place-mode)
+
+;; colorize dired listings (directories, symlinks, dates, file types)
+(use-package diredfl
+  :ensure t
+  :hook (dired-mode . diredfl-mode)
+  :config
+  ;; diredfl gives each permission-bit character (rwxrwxrwx) its own background
+  ;; color by default, which is too busy; strip those down to the default face
+  (dolist (face '(diredfl-read-priv diredfl-write-priv diredfl-exec-priv
+                  diredfl-dir-priv diredfl-no-priv diredfl-other-priv
+                  diredfl-rare-priv diredfl-link-priv diredfl-number
+                  diredfl-file-name diredfl-file-suffix))
+    (set-face-attribute face nil :foreground 'unspecified :background 'unspecified :inherit 'default)))
