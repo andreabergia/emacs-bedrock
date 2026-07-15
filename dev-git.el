06bad266a462571531e4600aeb484da4c39f2837
andrea.bergia <andrea.bergia@servicenow.com>
2026-07-15 11:36:51 +0200

    Magit should take over

--- dev-git.el
+++ dev-git.el
@@ -3,5 +3,8 @@
 ;; best Git client to ever exist
 (use-package magit
   :ensure t
-  :bind (("C-x g" . magit-status)))
+  :bind (("C-x g" . magit-status))
+  ;; open magit-status in the current frame (no window splitting), and
+  ;; restore the previous window layout when you quit magit (`q`)
+  :custom (magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
